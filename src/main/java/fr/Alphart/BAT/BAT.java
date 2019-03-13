36fed4fdb544897253b6f228099af9caa61c1713
OllieMartin <olivermartin410@gmail.com>
2019-03-13 18:31:35 +0000

    Refactored main class

--- src/main/java/fr/Alphart/BAT/BAT.java
+++ src/main/java/fr/Alphart/BAT/BAT.java
@@ -21,13 +21,6 @@ import java.util.logging.LogRecord;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.ProxyServer;
-import net.md_5.bungee.api.chat.BaseComponent;
-import net.md_5.bungee.api.chat.TextComponent;
-import net.md_5.bungee.api.connection.ProxiedPlayer;
-import net.md_5.bungee.api.plugin.Plugin;
-
 import com.google.common.base.Charsets;
 import com.google.common.base.Preconditions;
 import com.google.common.io.Files;
@@ -38,6 +31,12 @@ import fr.Alphart.BAT.Modules.Core.Core;
 import fr.Alphart.BAT.Utils.CallbackUtils.Callback;
 import fr.Alphart.BAT.Utils.RedisUtils;
 import fr.Alphart.BAT.database.DataSourceHandler;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.plugin.Plugin;
 
 /**
  * Main class BungeeAdminTools
@@ -45,9 +44,12 @@ import fr.Alphart.BAT.database.DataSourceHandler;
  * @author Alphart
  */
 public class BAT extends Plugin {
+
 	// This way we can check at runtime if the required BC build (or a higher one) is installed 
-	private final int requiredBCBuild = 878;
+	private static final int REQUIRED_BUNGEECORD_BUILD = 878;
+
 	private static BAT instance;
+
 	private static DataSourceHandler dsHandler;
 	private Configuration config;
 	private static String prefix;
@@ -56,135 +58,167 @@ public class BAT extends Plugin {
 
 	@Override
 	public void onEnable() {
+
 		instance = this;
 		config = new Configuration();
+
 		getLogger().setLevel(Level.INFO);
+
 		if (!ProxyServer.getInstance().getName().equals("BungeeCord")) {
-		  getLogger().warning("BungeeCord version check disabled because a fork has been detected."
-              + " Make sur your fork is based on a BungeeCord build > #" + requiredBCBuild);
+
+			getLogger().warning("BungeeCord version check disabled because a fork has been detected."
+					+ " Make sur your fork is based on a BungeeCord build > #" + REQUIRED_BUNGEECORD_BUILD);
+
+		} else if(getBCBuild() < REQUIRED_BUNGEECORD_BUILD && !new File(getDataFolder(), "skipversiontest").exists()) {
+
+			getLogger().severe("Your BungeeCord build (#" + getBCBuild() + ") is not supported. Please use at least BungeeCord #" + REQUIRED_BUNGEECORD_BUILD);
+			getLogger().severe("If you want to skip that test, create a file named 'skipversiontest' in BAT directory.");
+			getLogger().severe("BAT is going to shutdown ...");
+			return;
+
 		}
-		else if(getBCBuild() < requiredBCBuild && !new File(getDataFolder(), "skipversiontest").exists()){
-		  getLogger().severe("Your BungeeCord build (#" + getBCBuild() + ") is not supported. Please use at least BungeeCord #" + requiredBCBuild);
-		  getLogger().severe("If you want to skip that test, create a file named 'skipversiontest' in BAT directory.");
-          getLogger().severe("BAT is going to shutdown ...");
-          return;
+
+		if (config.isDebugMode()) {
+
+			try {
+
+				final File debugFile = new File(getDataFolder(), "debug.log");
+				if(debugFile.exists()){
+					debugFile.delete();
+				}
+
+				// Write header into debug log
+				Files.asCharSink(debugFile, Charsets.UTF_8).writeLines(Arrays.asList("BAT log debug file"
+						+ " - If you have an error with BAT, you should post this file on BAT topic on spigotmc",
+						"Bungee build : " + ProxyServer.getInstance().getVersion(),
+						"BAT version : " + getDescription().getVersion(),
+						"Operating System : " + System.getProperty("os.name"),
+						"Timezone : " + TimeZone.getDefault().getID(),
+						"------------------------------------------------------------"));
+
+				final FileHandler handler = new FileHandler(debugFile.getAbsolutePath(), true);
+				handler.setFormatter(new Formatter() {
+					private final SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+					private final String pattern = "time [level] message\n";
+					@Override
+					public String format(LogRecord record) {
+						return pattern.replace("level", record.getLevel().getName())
+								.replace("message", record.getMessage())
+								.replace("[BungeeAdminTools]", "")
+								.replace("time", sdf.format(Calendar.getInstance().getTime()));
+					}
+				});
+
+				getLogger().addHandler(handler);
+				getLogger().setLevel(Level.CONFIG);
+				getLogger().info("The debug mode is now enabled ! Logs are available in debug.log file located in BAT folder");
+				getLogger().config("Debug mode enabled ...");
+				getLogger().setUseParentHandlers(false);
+
+			} catch(final Exception e) {
+				getLogger().log(Level.SEVERE, "An exception occured during the initialization of debug logging file", e);
+			}
 		}
-		if(config.isDebugMode()){
-		    try{
-		        final File debugFile = new File(getDataFolder(), "debug.log");
-		        if(debugFile.exists()){
-		            debugFile.delete();
-		        }
-		        // Write header into debug log
-                Files.asCharSink(debugFile, Charsets.UTF_8).writeLines(Arrays.asList("BAT log debug file"
-                        + " - If you have an error with BAT, you should post this file on BAT topic on spigotmc",
-                        "Bungee build : " + ProxyServer.getInstance().getVersion(),
-                        "BAT version : " + getDescription().getVersion(),
-                        "Operating System : " + System.getProperty("os.name"),
-                        "Timezone : " + TimeZone.getDefault().getID(),
-                        "------------------------------------------------------------"));
-		        final FileHandler handler = new FileHandler(debugFile.getAbsolutePath(), true);
-		        handler.setFormatter(new Formatter() {
-		            private final SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-		            private final String pattern = "time [level] message\n";
-                    @Override
-                    public String format(LogRecord record) {
-                        return pattern.replace("level", record.getLevel().getName())
-                                    .replace("message", record.getMessage())
-                                    .replace("[BungeeAdminTools]", "")
-                                    .replace("time", sdf.format(Calendar.getInstance().getTime()));
-                    }
-                });
-		        getLogger().addHandler(handler);
-		        getLogger().setLevel(Level.CONFIG);
-		        getLogger().info("The debug mode is now enabled ! Log are available in debug.log file located in BAT folder");
-	            getLogger().config("Debug mode enabled ...");
-	            getLogger().setUseParentHandlers(false);
-		    }catch(final Exception e){
-		        getLogger().log(Level.SEVERE, "An exception occured during the initialization of debug logging file", e);
-		    }
-		}
+
 		prefix = config.getPrefix();
-		loadDB(new Callback<Boolean>(){
+
+		loadDB(new Callback<Boolean>() {
+
 			@Override
 			public void done(final Boolean dbState, Throwable throwable) {
 				if (dbState) {
-				    getLogger().config("Connection to the database established");
+					getLogger().config("Connection to the database established");
 					// Try enabling redis support.
 					redis = new RedisUtils(config.isRedisSupport());
-			        modules = new ModulesManager();
+					modules = new ModulesManager();
 					modules.loadModules();
 				} else {
-					getLogger().severe("BAT is gonna shutdown because it can't connect to the database.");
+					getLogger().severe("BAT must shutdown because it can't connect to the database.");
 					return;
 				}
 				// Init the I18n module
 				I18n.getString("global");
 			}
+
 		});
 	}
-	
-	public int getBCBuild(){
+
+	/**
+	 * @return The current BungeeCord build
+	 */
+	public int getBCBuild() {
+
 		final Pattern p = Pattern.compile(".*?:(.*?:){3}(\\d*)");
 		final Matcher m = p.matcher(ProxyServer.getInstance().getVersion());
 		int BCBuild;
-		try{
+
+		try {
 			if (m.find()) {
-			    BCBuild = Integer.parseInt(m.group(2));
+				BCBuild = Integer.parseInt(m.group(2));
 			}else{
 				throw new NumberFormatException();
 			}
-		}catch(final NumberFormatException e){
+		} catch(final NumberFormatException e) {
 			// We can't determine BC build, just display a message, and set the build so it doesn't trigger the security
 			getLogger().info("BC build can't be detected. If you encounter any problems, please report that message. Otherwise don't take into account");
-			BCBuild = requiredBCBuild;
+			BCBuild = REQUIRED_BUNGEECORD_BUILD;
 		}
+
 		return BCBuild;
 	}
 
 	@Override
 	public void onDisable() {
-	    if(redis != null){
-	      getRedis().destroy();
-	    }
-        modules.unloadModules();
+		if(redis != null){
+			getRedis().destroy();
+		}
+		modules.unloadModules();
 		instance = null;
 	}
 
+	/**
+	 * Load the database
+	 * @param dbState
+	 */
 	public void loadDB(final Callback<Boolean> dbState) {
+
 		if (config.isMysql_enabled()) {
-		    getLogger().config("Starting connection to the mysql database ...");
+
+			getLogger().config("Starting connection to the mysql database ...");
 			final String username = config.getMysql_user();
 			final String password = config.getMysql_password();
 			final String database = config.getMysql_database();
 			final String port = config.getMysql_port();
 			final String host = config.getMysql_host();
+
 			// BoneCP can accept no database and we want to avoid that
 			Preconditions.checkArgument(!"".equals(database), "You must set the database.");
+
 			ProxyServer.getInstance().getScheduler().runAsync(this, new Runnable() {
 				@Override
 				public void run() {
-				    try{
-				        dsHandler = new DataSourceHandler(host, port, database, username, password);
-	                    final Connection c = dsHandler.getConnection();
-	                    if (c != null) {
-                            c.close();
-                            dbState.done(true, null);
-                            return;
-	                    }
-				    }catch(final SQLException handledByDatasourceHandler){}
-				    getLogger().severe("The connection pool (database connection)"
-                            + " wasn't able to be launched !");
-                    dbState.done(false, null);
+					try{
+						dsHandler = new DataSourceHandler(host, port, database, username, password);
+						final Connection c = dsHandler.getConnection();
+						if (c != null) {
+							c.close();
+							dbState.done(true, null);
+							return;
+						}
+					}catch(final SQLException handledByDatasourceHandler){}
+					getLogger().severe("The connection pool (database connection)"
+							+ " wasn't able to be launched !");
+					dbState.done(false, null);
 				}
 			});
 		}
+
 		// If MySQL is disabled, we are gonna use SQLite
 		// Before initialize the connection, we must download the sqlite driver
 		// (if it isn't already in the lib folder) and load it
 		else {
-		    getLogger().config("Starting connection to the sqlite database ...");
-			getLogger().warning("It is strongly DISRECOMMENDED to use SQLite with BAT,"
+			getLogger().config("Starting connection to the sqlite database ...");
+			getLogger().warning("It is not recommended to use SQLite with BAT,"
 					+ " as the SQLite implementation is less stable and much slower than the MySQL implementation.");
 			if(loadSQLiteDriver()){
 				dsHandler = new DataSourceHandler();
@@ -195,7 +229,12 @@ public class BAT extends Plugin {
 		}
 	}
 
+	/**
+	 * Load the SQL Driver
+	 * @return TRUE if successfully enabled, FALSE otherwise
+	 */
 	public boolean loadSQLiteDriver(){
+
 		final File driverPath = new File(getDataFolder() + File.separator + "lib" + File.separator
 				+ "sqlite_driver.jar");
 		new File(getDataFolder() + File.separator + "lib").mkdir();
@@ -243,10 +282,19 @@ public class BAT extends Plugin {
 		}
 	}
 
+	/**
+	 * Get plugin instance
+	 * @return Instance of main plugin class
+	 */
 	public static BAT getInstance() {
 		return BAT.instance;
 	}
 
+	/**
+	 * This method appears to prepare a message for dispay with correct prefix and colour codes
+	 * @param message The message to display
+	 * @return Formatted message with prefix
+	 */
 	public static BaseComponent[] __(final String message) {
 		return TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&', prefix + message));
 	}
@@ -264,7 +312,7 @@ public class BAT extends Plugin {
 			BAT.getInstance().getRedis().sendBroadcast(perm, message);
 		}
 	}
-	
+
 	public static void noRedisBroadcast(final String message, final String perm) {
 		final BaseComponent[] bsMsg = __(message);
 		for (final ProxiedPlayer p : ProxyServer.getInstance().getPlayers()) {
@@ -284,24 +332,39 @@ public class BAT extends Plugin {
 		getInstance().getLogger().info(ChatColor.translateAlternateColorCodes('&', message));
 	}
 
+	/**
+	 * @return The modules manager of the plugin
+	 */
 	public ModulesManager getModules() {
 		return modules;
 	}
 
+	/**
+	 * @return The main config file of the plugin
+	 */
 	public Configuration getConfiguration() {
 		return config;
 	}
 
+	/**
+	 * @return The database connection
+	 */
 	public static Connection getConnection() {
 		return dsHandler.getConnection();
 	}
 
+	/**
+	 * @return The datasource handler
+	 */
 	public DataSourceHandler getDsHandler() {
 		return dsHandler;
 	}
 
+	/**
+	 * @return The Redis instance
+	 */
 	public RedisUtils getRedis() {
-	    return redis;
+		return redis;
 	}
 
 	/**
